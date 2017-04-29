2868285938b1856b9e1e0f34dd82482042657bad
James W Moody <jamesmoodyradio@gmail.com>
2017-04-28 22:29:59 -0500

    Add support for multiple spies, print out real names and aliases

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -7,63 +7,81 @@
 # -Print spy's new alias
 
 def alias_genrator
+  done = false
+  real_names = []
+  spy_names = []
+
   puts "Welcome to the super secret alias name generator. What is your full name?"
   real_name = gets.chomp.downcase
 
-  real_name_array = real_name.split(' ') # Turn name into array
+  until done
+    done = false
 
-  real_first_name = real_name_array[0].capitalize # Gets and capitalizes first string in full_name_array
-  real_last_name = real_name_array.last.capitalize
+    real_name_array = real_name.split(' ') # Turn name into array
 
-  real_names = []
-  real_names << real_first_name + " " + real_last_name # Stores real name to an array
+    real_first_name = real_name_array[0].capitalize # Gets and capitalizes first string in full_name_array
+    real_last_name = real_name_array.last.capitalize
 
-  name_letters = real_name.split('') # Turns user input into array of letters
+    real_names << real_first_name + " " + real_last_name # Stores real name to an array
 
-  vowels = ["a", "e", "i", "o", "u"]
-  consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z" ]
+    name_letters = real_name.split('') # Turns user input into array of letters
 
-  index = 0
-  name_letters.each do |letter| # Checks each letter in name_letters to see if the letter is in the vowels array
-    if letter == "u"
-      name_letters[index] = "a"
-    elsif
-      if vowels.include?(letter) # If it is, store the index of the vowel plus one
-        vowel_index = vowels.index(letter)+1
-        name_letters[index] = vowels[vowel_index] # Store the new letter in the corresponding index of name_letters
+    vowels = ["a", "e", "i", "o", "u"]
+    consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z" ]
+
+    index = 0
+    name_letters.each do |letter| # Checks each letter in name_letters to see if the letter is in the vowels array
+      if letter == "u"
+        name_letters[index] = "a"
+      elsif
+        if vowels.include?(letter) # If it is, store the index of the vowel plus one
+          vowel_index = vowels.index(letter)+1
+          name_letters[index] = vowels[vowel_index] # Store the new letter in the corresponding index of name_letters
+        end
       end
+      index+=1
     end
-    index+=1
+
+    index = 0
+    name_letters.each do |letter| # Checks each letter in name_letters to see if the letter is in the consonants array
+      if letter == "z"
+        name_letters[index] = "b"
+      elsif
+        if consonants.include?(letter) # If it is, store the index of the consonants plus one
+          consonant_index = consonants.index(letter)+1
+          name_letters[index] = consonants[consonant_index] # Store the new letter in the corresponding index of name_letters
+        end
+      end
+      index+=1
+    end
+
+    new_full_name = name_letters.join('').split(' ') # Joins the new letters of name_letters and splits it into two strings
+
+    # In case spy's real name includes a middle name
+    first_name = new_full_name[0].capitalize # Gets and capitalizes first string in new_full_name
+    last_name = new_full_name.last.capitalize # Gets and capitalizes last string in new_full_name
+
+    alias_name = last_name + " " + first_name
+
+    spy_names << alias_name # Stores alias into an array
+
+    p "Great, your cool new alias is #{alias_name}! Enter another name or type 'done' to exit."
+    answer = gets.chomp.downcase
+
+    if answer == "done" # Checks if user is done
+      done = true
+    else
+      real_name = answer
+    end
   end
 
-  index = 0
-  name_letters.each do |letter| # Checks each letter in name_letters to see if the letter is in the consonants array
-    if letter == "z"
-      name_letters[index] = "b"
-    elsif
-      if consonants.include?(letter) # If it is, store the index of the consonants plus one
-        consonant_index = consonants.index(letter)+1
-        name_letters[index] = consonants[consonant_index] # Store the new letter in the corresponding index of name_letters
-      end
-    end
-    index+=1
+  num_of_spies = real_names.length # Gets the number of spies
+  counter = 0
+
+  while counter < num_of_spies
+    p "#{spy_names[counter]} is really #{real_names[counter]}" # Prints spies real names and aliases
+    counter+=1
   end
-
-  new_full_name = name_letters.join('').split(' ') # Joins the new letters of name_letters and splits it into two strings
-
-  # In case spy's real name includes a middle name
-  first_name = new_full_name[0].capitalize # Gets and capitalizes first string in new_full_name
-  last_name = new_full_name.last.capitalize # Gets and capitalizes last string in new_full_name
-
-  alias_name = last_name + " " + first_name
-
-  spy_names = []
-  spy_names << alias_name # Stores alias into an array
-
-  p "Great, your cool new alias is #{alias_name}!"
-
-  p real_names
-  p spy_names
 end
 
 alias_genrator
