431419e1c10cb7e9f80d7bc49131da69188e6585
James W Moody <jamesmoodyradio@gmail.com>
2017-05-01 12:02:11 -0500

    Add characteristics and behaviors to puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# SPECIES ------------------------
+# Canis familiaris
+#
+# CHARACTERISTICS ----------------
+# Fur type: varies
+# Friendly: extremely
+# Nose: wet
+# Name: varies
+# Color: varies
+#
+# BEHAVIOR -----------------------
+# Bark
+# Jump
+# Rollover
+# Chase tail
