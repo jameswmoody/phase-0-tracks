be450945f657320e12321e16933e7cc0dc273afd
James W Moody <jamesmoodyradio@gmail.com>
2017-05-03 11:36:42 -0500

    Complete methods for release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,27 @@ class Puppy
     toy
   end
 
+  def speak(number)
+    puts "Woof!" * number
+  end
+
+  def rollover
+    puts "Rolls over."
+  end
+
+  def dog_years(human_years)
+    dog_years = human_years * 7
+  end
+
+  def play_dead
+    puts "......"
+  end
 end
 
+puppy = Puppy.new
+
+puppy.fetch("ball")
+puppy.speak(5)
+puppy.rollover
+puppy.dog_years(10)
+puppy.play_dead
