64aeda84611548a5ef3aef3d7a7f0160920c0c4c
James W Moody <jamesmoodyradio@gmail.com>
2017-05-06 12:53:23 -0500

    Add driver code to test methods, refactor code

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,65 +1,72 @@
 # Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
 # steps:
-  # Create empty hash to store items
-  # Split string into array of items
-  # Iterate over array to set each item as a key in hash
-  # set default quantity to 1
-  # print the list to the console using print method
+ # Create empty hash to store items
+ # Split string into array of items
+ # Iterate over array to set each item as a key in hash
+ # set default quantity to 1
+ # print the list to the console using print method
 # output: hash of items and quantites
 
 # Method to add an item to a list
 # input: list, item name, and optional quantity
 # steps:
-  # Add item name as a key to list hash
-  # Set optional quntity as value of item key
+ # Add item name as a key to list hash
+ # Set optional quntity as value of item key
 # output: hash with new item name and quantity added
 
 # Method to remove an item from the list
 # input: list, item name
 # steps:
-  # Remove item name from list hash
+ # Remove item name from list hash
 # output: hash with item removed
 
 # Method to update the quantity of an item
 # input: list, item name, quantity
 # steps:
-  # Set quantity as value of item name in list hash
+ # Set quantity as value of item name in list hash
 # output: hash with updated quantity for item
 
 # Method to print a list and make it look pretty
 # input: list
 # steps:
-  # For each item in list puts "Item Name: Quantity"
+ # For each item in list puts "Item Name: Quantity"
 # output: "Item Name: Quantity"
 
-def create_list(list)
-  grocery_list = {}
-  split_list = list.split
+$default_quantity = 1
 
-  split_list.each do |item|
-    grocery_list[item] = 1
-  end
-  print_list(grocery_list)
+def create_list(list)
+ grocery_list = {}
+ split_list = list.split
+
+ split_list.each { |item| grocery_list[item] = $default_quantity }
+ print_list(grocery_list)
+ grocery_list
 end
 
-def add_to_list(list, item, quantity = 1)
-  list[item] = quantity
-  list
+def add_to_list(list, item, quantity = $default_quantity)
+ update_quantity(list, item, quantity)
 end
 
 def remove_item(list, item)
-  list.delete(item)
-  list
+ list.delete(item)
+ list
 end
 
 def update_quantity(list, item, quantity)
-  list[item] = quantity
-  list
+ list[item] = quantity
+ list
 end
 
 def print_list(list)
-  list.each do |item, quantity|
-    puts "#{item}: #{quantity}"
-  end
+ list.each { |item, quantity| puts "#{item}: #{quantity}" }
 end
+
+grocery_list = create_list("cheese milk coffee apples")
+p add_to_list(grocery_list, "lemonade", 2)
+p add_to_list(grocery_list, "tomatoes", 3)
+p add_to_list(grocery_list, "onions")
+p add_to_list(grocery_list, "ice cream", 4)
+p remove_item(grocery_list, "lemonade")
+p update_quantity(grocery_list, "ice cream", 1)
+print_list(grocery_list)
