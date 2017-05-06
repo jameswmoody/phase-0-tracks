9fab740eafd72b2db6db10139558c8f0ecf19df0
James W Moody <jamesmoodyradio@gmail.com>
2017-05-06 01:21:01 -0500

    Build user interface for word game

--- ruby/word_game/word_game.rb
+++ ruby/word_game/word_game.rb
@@ -25,10 +25,10 @@
 # 		-Compare ‘user_guess’ to secret word
 
 class WordGame
-  attr_accessor :word, :guessed_letters
+  attr_accessor :secret_word, :guessed_letters
 
   def initialize(word)
-    @word = word.split ""
+    @secret_word = word.split ""
     @guessed_letters = []
   end
 
@@ -41,12 +41,17 @@ class WordGame
   end
 
   def out_of_guesses
-    @guessed_letters.length > @word.length
+    if @guessed_letters.length > @secret_word.length
+      puts "YOU LOSE!"
+      @guessed_letters.length > @secret_word.length
+    else
+      @guessed_letters.length > @secret_word.length
+    end
   end
 
   def censor
     censored_word = []
-    @word.each do |word_letter|
+    @secret_word.each do |word_letter|
       if @guessed_letters.include?(word_letter)
         censored_word << word_letter
       else
@@ -57,22 +62,57 @@ class WordGame
   end
 
   def check_for_match(user_guess)
-    user_guess == @word.join
+    user_guess == @secret_word.join
   end
 end
 
-word = WordGame.new("chill")
+# User interface
 
-# word.guess('c')
-# p word.censor
-# word.guess('h')
-# p word.censor
-# word.guess('y')
-# p word.censor
-# word.guess('l')
-# p word.censor
-# word.guess('l')
-# p word.censor
-# word.guess('l')
-# p word.censor
-# word.guess('i')
+puts "Welcome to the world famous super-duper Word Guessing Game! Type 'ready' to continue."
+
+until gets.chomp.downcase == "ready"
+  puts "Sorry, I didn't quite get that. Type 'ready' to continue."
+end
+
+puts "Great! User 1, enter a secret word. Make sure User 2 isn't peeking..."
+word = WordGame.new(gets.chomp.downcase)
+
+puts "OK! your secret word is locked in User 1. Now it's User 2's turn to guess. Type 'ready' to continue."
+
+until gets.chomp.downcase == "ready"
+  puts "Sorry, I didn't quite get that. Type 'ready' to continue."
+end
+
+puts "Hello User 2, the secret word is set and now it's time to guess."
+puts "Here's a hint: the word is #{word.secret_word.length} characters long. That means you'll have #{word.secret_word.length} attempts to get it right."
+puts "OK, let's go! Type 'ready' to start the game."
+
+until gets.chomp.downcase == "ready"
+  puts "Sorry, I didn't quite get that. Type 'ready' to continue."
+end
+
+until word.out_of_guesses
+  # Gets number of guesses left from differnce of word and guessed letter length
+  guesses_left = word.secret_word.length - word.guessed_letters.length
+
+  unless guesses_left < 1 # Unless the user is on their final guess
+    puts "Word: #{word.censor} Guesses left: #{guesses_left}"
+    puts "Pick a letter, or if you think you know the word, take a guess!"
+    guess = gets.chomp.downcase
+
+    if word.check_for_match(guess)
+      puts "Congratulations! You guessed it, the secret word was #{word.secret_word.join}."
+      break
+    end
+  else # Gives user one last shot at solving
+    puts "You are out of letters! Nothing left now but to try to solve the word..."
+    puts "Word: #{word.censor}"
+    guess = gets.chomp.downcase
+
+    if word.check_for_match(guess)
+      puts "Congratulations! You guessed it, the secret word was #{word.secret_word.join}."
+      break
+    end
+  end
+    word.guess(guess)
+end
